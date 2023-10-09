b194875c6c8e84041db67c6b37d8129b27761a73
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2023-10-09 09:06:17 -0300

    09.10 - 1

--- Clube-ciencias/script/APInoticia.js
+++ Clube-ciencias/script/APInoticia.js
@@ -1,20 +1,18 @@
 <script>
-        async function Noticias(){
-            var not = await fetch("https://run.mocky.io/v3/5b8bf8a2-3479-4142-8b3f-7d95a8a0472e", {
-                method: "GET"
-            })
+    async function Noticias(){
+        var not = await fetch("https://run.mocky.io/v3/5b8bf8a2-3479-4142-8b3f-7d95a8a0472e", {
+            method: "GET"
+        })
 
-            var respostaEmJson = await not.json()
-
-            document.getElementById("1h").innerText = respostaEmJson.titulo
-            document.getElementById("1p").innerText = respostaEmJson.subtitulo
-            document.getElementById("2p").innerText = respostaEmJson.conteudo
-            document.getElementById("2i").src = respostaEmJson.img2
-            document.getElementById("3h").innerText = respostaEmJson.data
-
-            
-            console.log(respostaEmJson)
-        }
-
-        Noticias()
-    </script>
+        var respostaEmJson = await not.json()
+        document.getElementById("1h").innerText = respostaEmJson.titulo
+        document.getElementById("1p").innerText = respostaEmJson.subtitulo
+        document.getElementById("2p").innerText = respostaEmJson.conteudo
+        document.getElementById("2i").src = respostaEmJson.img2
+        document.getElementById("3h").innerText = respostaEmJson.data
+        
+        console.log(respostaEmJson)
+    }
+    
+    Noticias()
+</script>
