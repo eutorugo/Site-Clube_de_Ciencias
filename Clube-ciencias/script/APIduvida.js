3de09ba0d3f3434c91d58576a5dbc88d240bb26f
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2023-10-09 12:17:14 -0300

    att

--- Clube-ciencias/script/APIduvida.js
+++ Clube-ciencias/script/APIduvida.js
@@ -1,4 +1,4 @@
-<script>
+
         async function Duvidas(){
             var not = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22duvidas%22%5D%7B%0A++%22id%22%3A_id%2C%0A++pergunta%2C%0A++++resposta%2C%0A%7D%0A", {
                 method: "GET"
@@ -12,4 +12,3 @@
             console.log(respostaEmJson)
         }
     Duvidas()
-    </script>
