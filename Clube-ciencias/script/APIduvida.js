ce8fcdae391562eac1d3fb96b30ea99336a078f4
MestreDoPortal <116765065+MestreDoPortal@users.noreply.github.com>
2023-10-17 21:41:32 -0300

    duvidas e noticias feito
    so falta arrumar os paragrafo e quebra de linha no noticia

--- Clube-ciencias/script/APIduvida.js
+++ Clube-ciencias/script/APIduvida.js
@@ -1,13 +1,42 @@
-    async function Duvidas(){
-        var not = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22duvidas%22%5D%7B%0A++%22id%22%3A_id%2C%0A++++pergunta%2C%0A++++resposta%0A%7D%0A", {
-            method: "GET"
-        })
-        var respostaEmJson = await not.json()
-        var dados = respostaEmJson.result[0]
+async function Duvidas() {
+    var duvidas = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22duvidas%22%5D%7B%0A++%22id%22%3A_id%2C%0A++++pergunta%2C%0A++++resposta%0A%7D%0A", {
+        method: "GET"
+    });
+    var respostaEmJson = await duvidas.json();
+    
+    // Selecione o contêiner onde você deseja exibir as perguntas e respostas
+    const duvidasContainer = document.getElementById("duvidasContainer");
 
-        document.getElementById("pergunta").querySelector("span").innerText = dados.pergunta
-        document.getElementById("collapseExample").querySelector(".card-body").innerText = dados.resposta;
+    // Itere por todas as perguntas e respostas na resposta da API
+    for (const duvida of respostaEmJson.result) {
+        // Crie elementos HTML para cada conjunto de pergunta e resposta
+        const perguntaDiv = document.createElement("p");
+        const botao = document.createElement("button");
+        botao.classList.add("campo-freq");
+        botao.type = "button";
+        botao.setAttribute("data-bs-toggle", "collapse");
+        botao.setAttribute("data-bs-target", `#collapse${duvida.id}`);
+        botao.setAttribute("aria-expanded", "false");
+        botao.setAttribute("aria-controls", `collapse${duvida.id}`);
+        const span = document.createElement("span");
+        span.innerText = duvida.pergunta;
+        botao.appendChild(span);
+        perguntaDiv.appendChild(botao);
 
-        console.log(respostaEmJson)
-        }
-    Duvidas()
+        const respostaDiv = document.createElement("div");
+        respostaDiv.classList.add("collapse");
+        respostaDiv.id = `collapse${duvida.id}`;
+        const card = document.createElement("div");
+        card.classList.add("card", "card-body");
+        card.innerText = duvida.resposta;
+        respostaDiv.appendChild(card);
+
+        // Adicione os elementos ao contêiner de dúvidas
+        duvidasContainer.appendChild(perguntaDiv);
+        duvidasContainer.appendChild(respostaDiv);
+    }
+
+    console.log(respostaEmJson);
+}
+
+Duvidas();
--- Clube-ciencias/script/APInoticia.js
+++ Clube-ciencias/script/APInoticia.js
@@ -1,17 +1,84 @@
-    async function Noticias(){
-        var not = await fetch("", {
+
+async function Noticias() {
+    var noticias = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22noticia%22%5D%7B%0A++%22id%22%3A_id%2C%0A++++titulo%2C%0A++++subtitulo%2C%0A++++conteudo%2C%0A++++%22imagem%22%3Aimagem.asset-%3Eurl%2C%0A++++data%0A%7D%0A", {
+        method: "GET"
+    });
+    var respostaEmJson = await noticias.json();
+
+    // Selecione o contêiner onde você deseja exibir as notícias
+    const noticiasContainer = document.getElementById("noticiasContainer");
+
+    // Itere por todas as notícias na resposta da API
+    for (const noticia of respostaEmJson.result) {
+        // Crie elementos HTML para cada conjunto de notícias
+        const noticiaDiv = document.createElement("div");
+        noticiaDiv.classList.add("noticia");
+
+        const txtDiv = document.createElement("div");
+        txtDiv.classList.add("txt");
+
+        const tituloP = document.createElement("p");
+        tituloP.classList.add("titulo");
+        tituloP.innerText = noticia.titulo;
+
+        const subtituloP = document.createElement("p");
+        subtituloP.classList.add("subtitulo");
+        subtituloP.innerText = noticia.subtitulo;
+
+        const descricaoP = document.createElement("p");
+        descricaoP.classList.add("descricao");
+        descricaoP.innerText = noticia.conteudo;
+
+        const dataP = document.createElement("p");
+        dataP.classList.add("descricao");
+        dataP.innerText = noticia.data;
+
+        const picture = document.createElement("picture");
+        const img = document.createElement("img");
+        img.classList.add("img-noticia");
+        img.alt = "";
+        img.src = noticia.imagem;
+
+        // Adicione os elementos ao contêiner de notícias
+        txtDiv.appendChild(tituloP);
+        txtDiv.appendChild(subtituloP);
+        txtDiv.appendChild(descricaoP);
+        txtDiv.appendChild(dataP);
+
+        picture.appendChild(img);
+
+        noticiaDiv.appendChild(txtDiv);
+        noticiaDiv.appendChild(picture);
+
+        noticiasContainer.appendChild(noticiaDiv);
+    }
+
+    console.log(respostaEmJson);
+}
+
+Noticias();
+
+
+
+
+
+
+
+
+/*async function Noticias(){
+        var not = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22noticia%22%5D%7B%0A++%22id%22%3A_id%2C%0A++++titulo%2C%0A++++subtitulo%2C%0A++++conteudo%2C%0A++++%22imagem%22%3Aimagem.asset-%3Eurl%2C%0A++++data%0A%7D%0A", {
             method: "GET"
         })
         var respostaEmJson = await not.json()
         var dados = respostaEmJson.result[0]
 
-        document.getElementById("").querySelector("").innerText = dados.titulo
-        document.getElementById("").querySelector("").innerText = dados.subtitulo
-        document.getElementById("").querySelector("").innerText = dados.conteudo
-        document.getElementById("").querySelector("").innerText = dados.img2
-        document.getElementById("").querySelector("").innerText = dados.data
+        document.getElementById("idtit").innerText = dados.titulo
+        document.getElementById("idsub").innerText = dados.subtitulo
+        document.getElementById("idcont").innerHTML = dados.conteudo
+        document.getElementById("idimg").src = dados.imagem
+        document.getElementById("iddata").innerText = dados.data
         
         console.log(respostaEmJson)
     }
 
-    Noticias()
+    Noticias()*/
