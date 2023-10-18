104bcc8b0e991abf2006b0f40a919070f62362fc
Matheus <matheusmorilha04@gmail.com>
2023-10-18 10:44:26 -0300

    Laboratorios & Equipe - PRONTO 18/10/2023

--- Clube-ciencias/script/APIlaboratorios.js
+++ Clube-ciencias/script/APIlaboratorios.js
@@ -1,47 +1,75 @@
-async function Laboratorios(){
-    var resultado = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27laboratorios%27%5D%7B%0A++++titulo%2C%0A++++descricao%2C%0A++++%22imagem%22%3Aimagem.asset-%3Eurl%0A%7D%0A",
-    {
+async function Laboratorios() {
+    var resultado = await fetch("https://ko6qqthj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27laboratorios%27%5D+%7C+order%28_createdAt%C2%A0asc%29%7B%0A++titulo%2C%0A++descricao%2C%0A++%22imagem%22%3Aimagem%5B%5D.asset-%3Eurl%0A%7D%0A", {
         method: "GET"
-    })
+    });
 
-    var respostaEmJson = await resultado.json()
-    var resultadoAPI = respostaEmJson.result
-    return resultadoAPI
-}
-async function APRESENTAR (){
-
-    var resultadoAPI = await Laboratorios()
-    console.log(resultadoAPI)
-
-    document.getElementById("tituloLab-1").innerText = resultadoAPI[4].titulo
-    document.getElementById("descLab-1").innerText = resultadoAPI[4].descricao
-    document.getElementById("imagemPre-1").src = resultadoAPI[4].imagem
- 
-    document.getElementById("tituloLab-2").innerText = resultadoAPI[1].titulo
-    document.getElementById("descLab-2").innerText = resultadoAPI[1].descricao
-
-
-    document.getElementById("tituloLab-3").innerText = resultadoAPI[2].titulo
-    document.getElementById("descLab-3").innerText = resultadoAPI[2].descricao
-
-
-    document.getElementById("tituloLab-4").innerText = resultadoAPI[0].titulo
-    document.getElementById("descLab-4").innerText = resultadoAPI[0].descricao
-
-
-    document.getElementById("tituloLab-5").innerText = resultadoAPI[3].titulo
-    document.getElementById("descLab-5").innerText = resultadoAPI[3].descricao
+    var respostaEmJson = await resultado.json();
+    var resultadoAPI = respostaEmJson.result;
+    return resultadoAPI;
 }
 
-APRESENTAR()
-   
+async function APRESENTAR() {
+    var resultadoAPI = await Laboratorios();
+    console.log(resultadoAPI);
 
+    const criarDivCards = document.querySelector(".cards");
 
+    for (let i = 0; i < resultadoAPI.length; i++) {
+        const laboratorios = resultadoAPI[i];
 
-    
+        const criarDivCard = document.createElement("div");
+        criarDivCard.className = "card";
 
+        if (i % 2 !== 0) {
+        criarDivCard.classList.add("reverse");
+        }
 
+        const criarDivTextos = document.createElement("div");
+        criarDivTextos.className = "txt";
 
+        const criarTitulo = document.createElement("p");
+        criarTitulo.className = "lab";
+        criarTitulo.innerText = laboratorios.titulo;
 
+        const criarDescricao = document.createElement("p");
+        criarDescricao.className = "desc-lab";
+        criarDescricao.innerText = laboratorios.descricao;
 
+        const criarDivImagens = document.createElement("div");
+        criarDivImagens.className = "card-img"; 
 
+        
+
+        for (let j = 0; j < laboratorios.imagem.length; j++) {
+            const imagemUrl = laboratorios.imagem[j];
+            const criarImagem = document.createElement("img");
+        
+            if (j === 0) {
+                criarImagem.src = imagemUrl;
+                criarImagem.style.width = "500px";
+                criarImagem.style.height = "300px";
+            } else {
+                criarImagem.src = imagemUrl;
+                criarImagem.style.width = "250px";
+                criarImagem.style.height = "300";
+            }
+        
+            criarDivImagens.appendChild(criarImagem);
+        
+            if (j >= laboratorios.imagem.length - 2) {
+                criarImagem.classList.add("stacked-image");
+            }
+        }
+        
+
+        criarDivTextos.appendChild(criarTitulo);
+        criarDivTextos.appendChild(criarDescricao);
+
+        criarDivCard.appendChild(criarDivTextos);
+        criarDivCard.appendChild(criarDivImagens);
+
+        criarDivCards.appendChild(criarDivCard);
+    }
+}
+
+APRESENTAR();
